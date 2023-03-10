b964dbcd2741d4aa0ec0a10698dc2e759674dbfc
chostler24 <connor.hostler@gmail.com>
2023-03-10 14:57:38 -0600

    Adding task 4

---
+++ 0x00-Javascript_advanced/4-math.js
@@ -0,0 +1,30 @@
+function divideBy(firstNumber) {
+    function secdivfir(secondNumber) {
+        return (secondNumber / firstNumber);
+    }
+    return secdivfir();
+};
+
+function addBy(firstNumber) {
+    function sumof(secondNumber) {
+        return (firstNumber + secondNumber);
+    }
+
+    return sumof();
+};
+
+{
+    const addBy100 = addBy(100);
+};
+
+{
+    const addBy1000 = addBy(1000);
+};
+
+{
+    const divideBy10 = divideBy(10);
+};
+
+{
+    const divideBy100 = divideBy(100);
+};
