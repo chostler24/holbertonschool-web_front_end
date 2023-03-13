5b47b9fd0c00c53e457da5141a2b225eec76d22e
chostler24 <connor.hostler@gmail.com>
2023-03-13 10:05:32 -0500

    task 4 completion

--- 0x00-Javascript_advanced/4-math.js
+++ 0x00-Javascript_advanced/4-math.js
@@ -1,30 +1,13 @@
 function divideBy(firstNumber) {
-    function secdivfir(secondNumber) {
-        return (secondNumber / firstNumber);
-    }
-    return secdivfir();
+    return (secondNumber) => {return (secondNumber/firstNumber);};
 };
 
 function addBy(firstNumber) {
-    function sumof(secondNumber) {
-        return (firstNumber + secondNumber);
-    }
+    return (secondNumber) => {return (secondNumber+firstNumber);};
+}
 
-    return sumof();
-};
+const addBy100 = addBy(100);
+const addBy1000 = addBy(1000);
+const divideBy10 = divideBy(10);
+const divideBy100 = divideBy(100);
 
-{
-    const addBy100 = addBy(100);
-};
-
-{
-    const addBy1000 = addBy(1000);
-};
-
-{
-    const divideBy10 = divideBy(10);
-};
-
-{
-    const divideBy100 = divideBy(100);
-};
