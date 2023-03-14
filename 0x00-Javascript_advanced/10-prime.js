8603265370ec06322beb437ceec7c6a8b99c3f86
chostler24 <connor.hostler@gmail.com>
2023-03-14 13:45:16 -0500

    Adding task 10

---
+++ 0x00-Javascript_advanced/10-prime.js
@@ -0,0 +1,26 @@
+let primenum = true;
+let count = 0;
+
+function countPrimeNumbers() {
+    let num;
+
+    for (let i = 2; i < 101; i++) {
+        for (i = 2; i < num; i++) {
+            if (num % i == 0) {
+                primenum = false;
+                break;
+            } else {
+                count++;
+            }
+        }
+    }
+
+    return count;
+}
+
+let start = performance.now();
+for (cnt = 1; cnt <= 100; cnt++) {
+    countPrimeNumbers();
+}
+let end = performance.now();
+console.log('Execution time of printing countPrimeNumbers was ' + (start - end) + ' milliseconds.');
