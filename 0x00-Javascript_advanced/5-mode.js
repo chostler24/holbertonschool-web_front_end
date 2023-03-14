028bc0714932647ad6ae1047fca12437393583ea
chostler24 <connor.hostler@gmail.com>
2023-03-14 11:03:52 -0500

    Adding tasks 5 and 6

--- 0x00-Javascript_advanced/5-mode.js
+++ 0x00-Javascript_advanced/5-mode.js
@@ -1,5 +1,11 @@
 function changeMode(size, weight, transform, background, color) {
-
+    return () => {
+        document.body.style.fontSize = size + 'px';
+        document.body.style.fontWeight = weight;
+        document.body.style.textTransform = transform;
+        document.body.style.backgroundColor = background;
+        document.body.style.color = color;
+    };
 }
 
 function main() {
--- 0x00-Javascript_advanced/6-hogwarts.js
+++ 0x00-Javascript_advanced/6-hogwarts.js
@@ -1,12 +1,40 @@
-function studentHogwarts() {
+const studentHogwarts = () => {
     let privateScore = 0;
     let name = null;
 
-    function changeScoreBy(points) {
+    const changeScoreBy = (points) => {
         privateScore += points;
     };
 
-    function setName(newName) {
-        let name;
-    }
-}
+    return {
+        setName: function (newName) {
+            name = newName;
+        },
+        rewardStudent: function () {
+            changeScoreBy(1);
+        },
+        penalizeStudent: function () {
+            changeScoreBy(-1);
+        },
+        getScore: function () {
+            return (name + ': ' + privateScore);
+        },
+    };
+};
+
+const harry = studentHogwarts();
+harry.setName("Harry");
+harry.rewardStudent();
+harry.rewardStudent();
+harry.rewardStudent();
+harry.rewardStudent();
+console.log(harry.getScore());
+
+
+let draco = studentHogwarts();
+draco.setName("Draco");
+draco.rewardStudent();
+draco.penalizeStudent();
+draco.penalizeStudent();
+draco.penalizeStudent();
+console.log(draco.getScore());
