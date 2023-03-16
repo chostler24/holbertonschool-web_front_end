374acc1b5ab62c3fb11d635252159138061e2e0d
chostler24 <connor.hostler@gmail.com>
2023-03-16 09:50:05 -0500

    got some closure

--- 0x00-Javascript_advanced/2-function_me.js
+++ 0x00-Javascript_advanced/2-function_me.js
@@ -1,7 +1,13 @@
 function welcomeMessage(fullName) {
-    alert('Welcome ' + fullName);
+    return () => {
+        alert('Welcome ' + fullName);
+    }
 }
 
 let guillaume = welcomeMessage('Guillaume');
 let alex = welcomeMessage('Alex');
 let fred = welcomeMessage('Fred');
+
+guillaume();
+alex();
+fred();
